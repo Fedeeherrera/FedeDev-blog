56dc032fe452ab1c990660011110483750add726
Fedeeherrera <Fedeeherrera2001@gmail.com>
2025-02-04 17:14:07 -0300

    cambios generales e instalacion de ShadCN

---
+++ app/components/NavBar.tsx
@@ -0,0 +1,12 @@
+import Link from 'next/link'
+
+function NavBar() {
+  return (
+    <nav className="w-full relative flex items-center justify-between max-w-2xl mx-auto py-5 px-4">
+      <Link href="/" className="text-2xl font-bold">
+        Fede<span className="text-blue-600">Dev</span>
+      </Link>
+    </nav>
+  )
+}
+export default NavBar
--- day-one-with-sanity/schemaTypes/blog.ts
+++ day-one-with-sanity/schemaTypes/blog.ts
@@ -1,12 +1,50 @@
 export default {
-  name : 'blog',
-  type : 'document',
-  title : 'blog',
-  fields : [
+  name: 'blog',
+  type: 'document',
+  title: 'blog',
+  fields: [
     {
-      name : 'title',
-      type : "string",
-      title : 'Title of blog article'
-    }
-  ]
+      name: 'title',
+      type: 'string',
+      title: 'Title of blog article',
+    },
+    {
+      name: 'slug',
+      type: 'slug',
+      title: 'Slug of your blog article',
+      options: {
+        source: 'title',
+      },
+    },
+    {
+      name: 'titleImage',
+      type: 'image',
+      title: 'Title Image',
+    },
+    {
+      name: 'linkWebsite',
+      type: 'string',
+      title: 'Link to Website',
+    },
+    {
+      name: 'linkGithubRepo',
+      type: 'string',
+      title: 'Link to Github Repo',
+    },
+    {
+      name: 'smallDescription',
+      type: 'text',
+      title: 'Small Description',
+    },
+    {
+      name: 'content',
+      type: 'array',
+      title: 'Content',
+      of: [
+        {
+          type: 'block',
+        },
+      ],
+    },
+  ],
 }
