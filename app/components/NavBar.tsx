2185e5972776604e842598e7631974c0bdc5bf2d
Fedeeherrera <Fedeeherrera2001@gmail.com>
2025-02-04 23:11:16 -0300

    changes

--- app/components/NavBar.tsx
+++ app/components/NavBar.tsx
@@ -4,8 +4,11 @@ function NavBar() {
   return (
     <nav className="w-full relative flex items-center justify-between max-w-2xl mx-auto py-5 px-4">
       <Link href="/" className="text-2xl font-bold">
-        Fede<span className="text-blue-600">Dev</span>
+        Fede<span className="text-primary">Dev</span>
       </Link>
+
+
+
     </nav>
   )
 }
