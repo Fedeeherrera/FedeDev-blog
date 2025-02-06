0593faf9866f998eafecb7ceb690c959d09ee3e6
Fedeeherrera <Fedeeherrera2001@gmail.com>
2025-02-06 15:46:46 -0300

    show post, interface and sanity querys

--- app/page.tsx
+++ app/page.tsx
@@ -1,17 +1,46 @@
+import { Card, CardContent} from '@/components/ui/card'
+import { simpleBlogCard } from './lib/interface'
+import { Button } from '@/components/ui/button'
+import { client, urlFor } from './lib/sanity'
+import Image from 'next/image'
+import Link from 'next/link'
+
 async function getData() {
-  const res = await fetch('https://jsonplaceholder.typicode.com/posts')
-  const data = await res.json()
+  const query = `*[_type == 'blog'] | order(_createdAt desc) {
+  title,
+    smallDescription,
+    "currentSlug" : slug.current,
+    titleImage
+}`
+
+  const data = await client.fetch(query)
   return data
 }
 
-function page() {
-  const data = getData()
+async function Home() {
+  const data: simpleBlogCard[] = await getData()
   return (
-    <div>
-      <h1>Hello World</h1>
-      <pre>{JSON.stringify(data, null, 2)}</pre>
+    <div className="grid grid-cols-1 md:grid-cols-2 mt-5 gap-5">
+      {data.map((post, index) => (
+        <Card key={index}>
+          <Image
+            src={urlFor(post.titleImage).url()}
+            alt="Imagen"
+            width={500}
+            height={500}
+            className='roundex-t-lg h-[200px] object-cover'
+          />
+          <CardContent className='mt-5'>
+            <h3 className='text-lg line-clamp-2 font-bold'>{post.title}</h3>
+            <p className='line-clamp-3 text-sm mt-2 text-gray-600 dark:text-gray-400'>{post.smallDescription}</p>
+            <Button asChild className='w-full mt-7'>
+          <Link href={`/blog/${post.currentSlug}`}>Read more</Link>
+            </Button>
+          </CardContent>
+        </Card>
+      ))}
     </div>
   )
 }
 
-export default page
+export default Home
