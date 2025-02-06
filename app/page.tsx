035b393005a851cb0ebe4e8fb74d677d9ade0ae4
Fedeeherrera <fedeeherrera2001@gmail.com>
2025-02-06 20:21:50 -0300

    agregue contenido a sanity

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,4 @@
-import { Card, CardContent} from '@/components/ui/card'
+import { Card, CardContent } from '@/components/ui/card'
 import { simpleBlogCard } from './lib/interface'
 import { Button } from '@/components/ui/button'
 import { client, urlFor } from './lib/sanity'
@@ -28,13 +28,15 @@ async function Home() {
             alt="Imagen"
             width={500}
             height={500}
-            className='roundex-t-lg h-[200px] object-cover'
+            className="roundex-t-lg h-[200px] object-cover"
           />
-          <CardContent className='mt-5'>
-            <h3 className='text-lg line-clamp-2 font-bold'>{post.title}</h3>
-            <p className='line-clamp-3 text-sm mt-2 text-gray-600 dark:text-gray-400'>{post.smallDescription}</p>
-            <Button asChild className='w-full mt-7'>
-          <Link href={`/blog/${post.currentSlug}`}>Read more</Link>
+          <CardContent className="mt-5">
+            <h3 className="text-lg line-clamp-2 font-bold">{post.title}</h3>
+            <p className="line-clamp-3 text-sm mt-2 text-gray-600 dark:text-gray-400">
+              {post.smallDescription}
+            </p>
+            <Button asChild className="w-full mt-7">
+              <Link href={`/blog/${post.currentSlug}`}>Read more</Link>
             </Button>
           </CardContent>
         </Card>
